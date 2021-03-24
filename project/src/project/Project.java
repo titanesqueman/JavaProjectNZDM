19bedf7537f3f2872c008535dd671a64592f2f1a
titanesqueman <nicolas_z@hotmail.fr>
2021-03-24 17:53:18 +0100

    a=3

--- project/src/project/Project.java
+++ project/src/project/Project.java
@@ -16,8 +16,7 @@ public class Project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // A FAIRE
-        // drazic
+        int a = 3;
     }
     
 }
