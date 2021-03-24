c7f506db3132593b645674d8999d8e370995d315
titanesqueman <nicolas_z@hotmail.fr>
2021-03-24 17:16:46 +0100

    test

---
+++ JavaApplication14/src/project/Project.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author nicol
+ */
+public class Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    //SUPER TEST DE LA MORT QUI TUE
+    //test 2
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
